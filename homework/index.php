b94bff1b909ebb59d1c1081e09acf28cc77b1676
Jason Sagisi <jsagisi@csumb.edu>
2017-10-11 20:09:49 +0000

    a

---
+++ homework/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        In Homework
+        <div>
+            
+            </br>
+            <a href ="/homework1/home.html"> Homework1</a>
+                
+        <div>
+            
+        </div>
+    </body>
+</html>
