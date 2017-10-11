649070494b4d1dd1a119145deeb139e53e6ba622
Jason Sagisi <jsagisi@csumb.edu>
2017-10-11 19:56:19 +0000

    proper index

--- homework/index.php
+++ homework/index.php
@@ -1,1 +1,17 @@
-in Homework
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        In Homework
+        <div>
+            
+            </br>
+            <a href ="/homework1"> Homework1</a>
+                
+        <div>
+            
+        </div>
+    </body>
+</html>
