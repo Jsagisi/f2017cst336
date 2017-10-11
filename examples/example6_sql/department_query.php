1ab11b3a956bf97c435d8ee8388f594d008c50e6
Jason Sagisi <jsagisi@csumb.edu>
2017-10-11 22:06:30 +0000

    Index1

---
+++ examples/example6_sql/department_query.php
@@ -0,0 +1,22 @@
+<?php
+
+$dbHost = getenv('IP');
+$dbname = "sql_example";
+$dbPort = 3306;
+$username = getenv('C9_USER');
+$password = "";
+
+$dbConn = new PDO("mysql:host=$dbHost;port=$dbPort;dbname=$dbname", $username, $password);
+$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql = "SELECT * FROM employee WHERE employee_id = 123";
+
+
+$stmt = $dbConn -> prepare ($sql);
+$stmt -> execute ();
+
+while ($row = $stmt -> fetch()){
+    echo $row['employee_name'];    
+}
+
+?>
