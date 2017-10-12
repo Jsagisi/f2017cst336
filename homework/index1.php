b68e9064ffd34935ab45d09de8ecb43f11a23929
Jason Sagisi <jsagisi@csumb.edu>
2017-10-12 23:19:11 +0000

    Workspace Fix

--- homework/index1.php
+++ homework/index1.php
@@ -1,1 +1,18 @@
-In Homework 1
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <div>
+            In Homework Folder
+            <a href="/homework3">Homework 3</a>
+            </br>
+          
+                
+        <div>
+            
+        </div>
+    </body>
+</html>
