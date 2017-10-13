9fd855413f5f6215288f6c1b7cb538e9f0206bd1
Jason Sagisi <jsagisi@csumb.edu>
2017-10-13 19:35:08 +0000

    Fixed Index.php to show on heroku

--- index.php
+++ index.php
@@ -11,7 +11,27 @@
         <div>
             <a href="/lab">Lab</a>
         </div>
-            <a href="homework/homework2/baby_name.php">Homework</a>
-        
+            Homeworks
+            </br>
+            
+            <a href="homework/homework1/home.html">Homework 1</a>
+            </br>
+            <a href="homework/homework2/baby_name.php">Homework 1</a>
+            </br>
+            <a href="homework/homework3/quiz.php">Homework 3</a>
+            </br>
+            </br>
+            </br>
+            </br>
+            Labs
+            <a href="lab/lab1/home.html">Lab 1</a>
+            </br>
+            <a href="lab/lab2/index.php">Lab 2</a>
+            </br>
+            <a href="lab/lab3/silverjack.php">Lab 3</a>
+            </br>
+            <a href="lab/lab4/lab4.php">Lab 4</a>
+            </br>
+            
     </body>
 </html>
