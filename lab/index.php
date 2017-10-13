0681b1bf91074f22fd5e28fdc0fe231761e2ba2e
Jason Sagisi <jsagisi@csumb.edu>
2017-10-13 18:39:35 +0000

    Directory

--- lab/index.php
+++ lab/index.php
@@ -6,6 +6,8 @@
     <body>
         In Labs Folder
         </br>
-        <a href="/lab/lab4">Lab 4</a>
+        <a href="/lab/lab4">Lab 4 (lab/lab4)</a>
+        </br>
+        <a href="workspace/lab/lab3"> Lab 3 (workspace/lab/lab3</a>
     </body>
 </html>
